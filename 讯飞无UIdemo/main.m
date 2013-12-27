b0d9f54f1084e55dc37ee66b8e87f40cf9924e77
ZhangCheng <zhangcheng@ZhangChengdeMacBook-Pro.local>
2013-12-27 09:00:33 +0800

    Initial Commit

---
+++ 讯飞无UIdemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  讯飞无UIdemo
+//
+//  Created by ZhangCheng on 13-12-27.
+//  Copyright (c) 2013年 ZhangCheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ 讯飞无UIdemoTests/___UIdemoTests.m
@@ -0,0 +1,34 @@
+//
+//  ___UIdemoTests.m
+//  讯飞无UIdemoTests
+//
+//  Created by ZhangCheng on 13-12-27.
+//  Copyright (c) 2013年 ZhangCheng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ___UIdemoTests : XCTestCase
+
+@end
+
+@implementation ___UIdemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
